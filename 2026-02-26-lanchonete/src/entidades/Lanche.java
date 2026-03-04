884992f18bc2420436a1982cb8a864b92ee5e082
Matheus Lucindo <matheuslucindo904@gmail.com>
2026-03-04 19:20:48 -0300

    Atividades de java

--- 2026-02-26-lanchonete/src/entidades/Lanche.java
+++ 2026-02-26-lanchonete/src/entidades/Lanche.java
@@ -21,25 +21,39 @@ public class Lanche {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public double getPreco() {
         return preco;
     }
 
     public void setPreco(double preco) {
-        if (preco > 0) this.preco = preco;
+        if (preco <= 0) {
+            System.out.println("Preço inválido. O preço deve ser maior que zero.");
+        } else {
+            this.preco = preco;
+            System.out.println("Preço atualizado para R$ " + preco);
+        }
     }
 
     public int getEstoque() {
-        return estoque;
+        return estoque; // Busca estoque atual
     }
 
     public void setEstoque(int estoque) {
         this.estoque = estoque;
         this.disponivel = estoque > 0; // Atualiza disponibilidade ao mudar estoque
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+
+    }
+
+    public double precoTotal(int quantidade) {
+        return this.preco * quantidade;
     }
 
     @Override
